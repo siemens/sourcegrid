782333e1a21d96f8f5f023fbf234079ae9c9ce56
Rahul-Siemens <144675885+Rahul-Siemens@users.noreply.github.com>
2024-07-12 09:15:02 +0530

    build: migrate sample forms to net8 (#38)

--- Src/DevAge.Windows.Forms/Properties/Resources.Designer.cs
+++ Src/DevAge.Windows.Forms/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace DevAge.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DevAge.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sourcegrid.DevAge.Windows.Forms.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
